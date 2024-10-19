aea49193997ff743d2e3d7514bdf829c294e5604
BLAST3R-PB2 <miribince@gmail.com>
2024-10-19 13:34:43 +0300

    optimized FPS and file size

--- create_pb2_map.py
+++ create_pb2_map.py
@@ -16,17 +16,24 @@ y_offset = -(height_pixels // 2) * config.PIXEL_SIZE
 xml_data = []
 
 PLAYER_NAME = 'BLAST3R'
+ZOOM_VALUE = 100
 
 # player, camera, and concrete ground data
-player_data_xml = f'<player uid="#player*1" x="270" y="170" tox="0" toy="0" hea="500" hmax="500" team="0" side="1" char="77" incar="-1" botaction="0" ondeath="-1" /><box x="-500" y="170" w="1100" h="1230" m="0" /><trigger uid="#trigger*9999999" x="310" y="20" enabled="true" maxcalls="1" actions_1_type="51" actions_1_targetA="120" actions_1_targetB="0" actions_2_type="-1" actions_2_targetA="0" actions_2_targetB="0" actions_3_type="52" actions_3_targetA="#player*1" actions_3_targetB="BLAST3R" actions_4_type="-1" actions_4_targetA="0" actions_4_targetB="0" actions_5_type="-1" actions_5_targetA="0" actions_5_targetB="0" actions_6_type="-1" actions_6_targetA="0" actions_6_targetB="0" actions_7_type="-1" actions_7_targetA="0" actions_7_targetB="0" actions_8_type="-1" actions_8_targetA="0" actions_8_targetB="0" actions_9_type="-1" actions_9_targetA="0" actions_9_targetB="0" actions_10_type="-1" actions_10_targetA="0" actions_10_targetB="0" /><timer uid="#timer*9999999" x="310" y="-10" enabled="true" maxcalls="1" target="#trigger*9999999" delay="0" />'
+player_data_xml = f'<player uid="#player*1" x="270" y="170" tox="0" toy="0" hea="500" hmax="500" team="0" side="1" char="77" incar="-1" botaction="0" ondeath="-1" /><box x="-500" y="170" w="1100" h="1230" m="0" /><trigger uid="#trigger*9999999" x="310" y="20" enabled="true" maxcalls="1" actions_1_type="51" actions_1_targetA="{ZOOM_VALUE}" actions_1_targetB="0" actions_2_type="52" actions_2_targetA="#player*1" actions_2_targetB="{PLAYER_NAME}"/><timer uid="#timer*9999999" x="310" y="-10" enabled="true" maxcalls="1" target="#trigger*9999999" delay="0" />'
 
 xml_data.append(player_data_xml)
 
-# Create 2 triggers per pixel, the first one turns it white, the second one turns it black
-
-# coords to start placing the triggers & timers
-y_value = -700
-x_value = -700
+def number_to_letters(n): # more sophisticated base-52 numbering method for shorter id names
+    characters = [chr(i) for i in range(ord('a'), ord('z')+1)] + [chr(i) for i in range(ord('A'), ord('Z')+1)]
+    base = len(characters)
+    result = ''
+    
+    while n > 0:
+        n -= 1  # Adjust to 0-based indexing
+        result = characters[n % base] + result
+        n //= base
+    
+    return result
 
 # Iterate through the grid
 uid = 1  # Start UID for each movable (door)
@@ -38,27 +45,24 @@ for i in range(height_pixels):
         y = i * config.PIXEL_SIZE + y_offset
         
         # Create the XML element for the movable
-        xml_element = f'<door uid="#d{uid}" x="{x}" y="{y}" w="{config.PIXEL_SIZE}" h="{config.PIXEL_SIZE}" tarx="0" tary="0" vis="true"/>'
-        pixel_trigger_list_white = [f'actions_1_type="71" actions_1_targetA="#d{uid}" actions_1_targetB="#FFFFFF"']
-        pixel_trigger_list_black = [f'actions_1_type="71" actions_1_targetA="#d{uid}" actions_1_targetB="#0"']
+        xml_element = f'<door uid="#*{number_to_letters(uid)}" x="{x}" y="{y}" w="{config.PIXEL_SIZE}" h="{config.PIXEL_SIZE}" tarx="0" tary="0" vis="true"/>'
+        
+        # Create 2 triggers per pixel, the first one turns it white, the second one turns it black
+        pixel_trigger_list_white = [f'actions_1_type="71" actions_1_targetA="#*{number_to_letters(uid)}" actions_1_targetB="#FFFFFF"']
+        pixel_trigger_list_black = [f'actions_1_type="71" actions_1_targetA="#*{number_to_letters(uid)}" actions_1_targetB="#0"']
         
         white_trigger_actions = " ".join(pixel_trigger_list_white)
         black_trigger_actions = " ".join(pixel_trigger_list_black)
 
-        white_trigger_element = f'<trigger uid="#{trigger_uid}" x="{x_value}" y="{y_value}" enabled="true" maxcalls="-1" {white_trigger_actions}/>'
-        black_trigger_element = f'<trigger uid="#{trigger_uid + 1}" x="{x_value - 50}" y="{y_value}" enabled="true" maxcalls="-1" {black_trigger_actions}/>'
+        white_trigger_element = f'<trigger uid="#{number_to_letters(trigger_uid)}" enabled="true" maxcalls="-1" {white_trigger_actions}/>'
+        black_trigger_element = f'<trigger uid="#{number_to_letters(trigger_uid + 1)}" enabled="true" maxcalls="-1" {black_trigger_actions}/>'
 
         xml_data.append(xml_element)
         xml_data.append(white_trigger_element)
         xml_data.append(black_trigger_element)
 
-        y_value += 50
-
         trigger_uid += 2
         uid += 1  # Increment UID for the next movable
-    
-    x_value -= 100
-    y_value = -700
 
 # Write to an XML file
 with open(f'{FILE_NAME}', 'w') as file:
@@ -84,29 +88,13 @@ timer_data_xml = []
 
 # timer values to sync framerate in PB2 with 30fps actual framerate
 timer_delay_delta = (30//config.FRAME_RATE)
-timer_delay_value = timer_delay_delta # initial wait is 10 sec (CHANGE THIS)
-
-# coords to start placing the timers
-y_value = -700
-x_value = -4000
+timer_delay_value = timer_delay_delta # initial wait time
 
 # uid values will be incremented after every timer has been placed
 uid = 1
 
 # keeping track of previous frame to avoid changing pixels when not needed (saves data)
 old_frames_list = [0] * config.ASPECT_RATIO_WIDTH * config.ASPECT_RATIO_HEIGHT
-
-def number_to_letters(n):
-    characters = [chr(i) for i in range(ord('a'), ord('z')+1)] + [chr(i) for i in range(ord('A'), ord('Z')+1)]
-    base = len(characters)
-    result = ''
-    
-    while n > 0:
-        n -= 1  # Adjust to 0-based indexing
-        result = characters[n % base] + result
-        n //= base
-    
-    return result
 
 for frame in range(len(frame_data_files)): # start reading data from every frame 
 
@@ -121,15 +109,12 @@ for frame in range(len(frame_data_files)): # start reading data from every frame
         # Only create timer_element if the pixel value changed from the old frame
         if frame_data_content[i] != old_frames_list[i]:
             if frame_data_content[i] == "1":  # Make it white
-                timer_element = f'<timer uid="#{number_to_letters(uid)}" x="{0}" y="{0}" enabled="true" maxcalls="1" target="#{(i+1)*2-1}" delay="{timer_delay_value}"/>'
+                timer_element = f'<timer enabled="true" maxcalls="1" target="#{number_to_letters((i+1)*2-1)}" delay="{timer_delay_value}"/>'
             else:  # Make it black
-                timer_element = f'<timer uid="#{number_to_letters(uid)}" x="{0}" y="{0}" enabled="true" maxcalls="1" target="#{(i+1)*2}" delay="{timer_delay_value}"/>'
+                timer_element = f'<timer enabled="true" maxcalls="1" target="#{number_to_letters((i+1)*2)}" delay="{timer_delay_value}"/>'
             
             # Append the timer element only when there's a change
             timer_data_xml.append(timer_element)
-
-            # place the leading triggers below the trigger that came before
-            y_value += 50
 
             # increment the uid to make sure the ids are unique
             uid += 1
@@ -137,12 +122,8 @@ for frame in range(len(frame_data_files)): # start reading data from every frame
     # Increase the timer delay to maintain the 30 FPS rate
     timer_delay_value += timer_delay_delta
 
-    # copy the triggers from the last frame to be able to compare them with the next
-    old_frames_list = frame_data_content
-
-    # shift the triggers left per frame
-    x_value -= 100
-    y_value = -700
+    # copy the data from the last frame to be able to compare them with the next
+    old_frames_list = frame_data_content.copy()
 
 
 # append all changes made.
